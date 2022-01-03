f04de5066593afc74d2b1cd64649cdd98909824c
Ville Koskela <vkoskela@gmail.com>
2022-01-03 21:08:40 +0200

    started 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import useRepositories from '../hooks/useRepositories';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
@@ -6,7 +6,9 @@ import RepositoryListContainer from './RepositoryListContainer';
 
 const RepositoryList = () => {
 
-  const { data, loading } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' });
+  const [orderBy, setOrderBy] = useState('CREATED_AT');
+  const [direction, setDirection] = useState('DESC');
+  const { data, loading } = useQuery(GET_REPOSITORIES, { variables: { orderBy: orderBy, orderDirection: direction }, fetchPolicy: 'cache-and-network' });
 
   if (loading) {
     return <></>;
